8d600d2f23474851adc4368df2c1b0ed96d93937
Ellian Carlos <elliancarlos@gmail.com>
2025-05-10 22:40:15 -0300

    feat: add new home-managers

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -11,7 +11,13 @@
   # environment.
   home.packages =  with pkgs; [
     oh-my-zsh
+
+    xclip
+    xsel
+
+    zsh-powerlevel10k
   ];
+
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
@@ -35,17 +41,67 @@
 
   programs.zsh = {
     enable = true;
+
+    localVariables = {
+      ZSH_DISABLE_COMPFIX = "true";
+    };
+
     shellAliases = {
       la = "ls -la";
       update = "sudo nixos-rebuild switch";
     };
+
+
     autosuggestion.enable = true;
-    
+    enableCompletion = true;
+    syntaxHighlighting.enable = true;
+
+    zplug = {
+        enable=true;
+        plugins = [
+          { name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; } # Installations with additional options. For the list of options, please refer to Zplug README.
+        ];
+    };
+
+    initExtra = '' 
+      [[ ! -f ${./.p10k.zsh} ]] || source ${./.p10k.zsh}
+    '';
+
     oh-my-zsh = {
       enable = true;
-      plugins = ["git"];
+
+      plugins = [
+        "git"
+        "fzf"
+        "z"
+        "copyfile"
+        "history"
+        "dirhistory"
+      ];
+
       theme = "robbyrussell";
     };
+
+    plugins = [
+        {
+          name = "zsh-completions";
+          src = pkgs.fetchFromGitHub {
+            owner = "zsh-users";
+            repo = "zsh-completions";
+            rev = "0.35.0";
+            hash = "sha256-GFHlZjIHUWwyeVoCpszgn4AmLPSSE8UVNfRmisnhkpg=";
+          };
+        }
+        {
+          name = "zsh-syntax-highlighting";
+          src = pkgs.fetchFromGitHub {
+            owner = "zsh-users";
+            repo = "zsh-syntax-highlighting";
+            rev = "0.8.0";
+            hash = "sha256-iJdWopZwHpSyYl5/FQXEW7gl/SrKaYDEtTH9cGP7iPo=";
+          };
+        }
+    ];
   };
 
   programs.wofi = {
