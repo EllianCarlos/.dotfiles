cda561ffb85436efef6bb847dd4094c70484f62a
Ellian Carlos <elliancarlos@gmail.com>
2025-05-10 22:54:24 -0300

    fix: show only last file on zsh path file

--- .config/home-manager/.p10k.zsh
+++ .config/home-manager/.p10k.zsh
@@ -189,5 +189,7 @@
 # Tell `p10k configure` which file it should overwrite.
 typeset -g POWERLEVEL9K_CONFIG_FILE=${${(%):-%x}:a}
 
+typeset -g POWERLEVEL9K_SHORTEN_STRATEGY=truncate_to_last
+
 (( ${#p10k_config_opts} )) && setopt ${p10k_config_opts[@]}
 'builtin' 'unset' 'p10k_config_opts'
